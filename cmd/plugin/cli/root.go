d8c42b1fc86c1715bcf78c9606fbaca4034fee24
Bekir Dogan <bekirdo@gmail.com>
2020-02-27 05:00:54 +0000

    real initial commit

--- cmd/plugin/cli/root.go
+++ cmd/plugin/cli/root.go
@@ -4,77 +4,55 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
-	"github.com/bergerx/kubectl-resource-status/pkg/logger"
 	"github.com/bergerx/kubectl-resource-status/pkg/plugin"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/tj/go-spin"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-)
-
-var (
-	KubernetesConfigFlags *genericclioptions.ConfigFlags
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
 
 func RootCmd() *cobra.Command {
+	KubernetesConfigFlags := genericclioptions.
+		NewConfigFlags(false)
+	ResourceBuilderFlags := genericclioptions.
+		NewResourceBuilderFlags().
+		WithAll(false).
+		WithAllNamespaces(false).
+		WithFile(false).
+		WithLabelSelector("").
+		WithLatest()
+
+	f := cmdutil.NewFactory(KubernetesConfigFlags)
+
 	cmd := &cobra.Command{
-		Use:           "resource-status",
-		Short:         "",
+		Use:           "TODO-BINARY-NAME (TYPE[.VERSION][.GROUP] [NAME | -l label] | TYPE[.VERSION][.GROUP]/NAME ...) [flags]",
+		//DisableFlagsInUseLine: true,
+		Short:         "Display resource-status for one or many resources",
 		Long:          `.`,
 		SilenceErrors: true,
 		SilenceUsage:  true,
 		PreRun: func(cmd *cobra.Command, args []string) {
 			viper.BindPFlags(cmd.Flags())
 		},
-		RunE: func(cmd *cobra.Command, args []string) error {
-			log := logger.NewLogger()
-			log.Info("")
-
-			s := spin.New()
-			finishedCh := make(chan bool, 1)
-			namespaceName := make(chan string, 1)
-			go func() {
-				lastNamespaceName := ""
-				for {
-					select {
-					case <-finishedCh:
-						fmt.Printf("\r")
-						return
-					case n := <-namespaceName:
-						lastNamespaceName = n
-					case <-time.After(time.Millisecond * 100):
-						if lastNamespaceName == "" {
-							fmt.Printf("\r  \033[36mSearching for namespaces\033[m %s", s.Next())
-						} else {
-							fmt.Printf("\r  \033[36mSearching for namespaces\033[m %s (%s)", s.Next(), lastNamespaceName)
-						}
-					}
-				}
-			}()
-			defer func() {
-				finishedCh <- true
-			}()
-
-			if err := plugin.RunPlugin(KubernetesConfigFlags, namespaceName); err != nil {
-				return errors.Cause(err)
-			}
-
-			log.Info("")
-
-			return nil
+		Run: func(cmd *cobra.Command, args []string) {
+			cmdutil.CheckErr(Run(f, cmd, args))
 		},
 	}
+	KubernetesConfigFlags.AddFlags(cmd.Flags())
+	ResourceBuilderFlags.AddFlags(cmd.Flags())
 
 	cobra.OnInitialize(initConfig)
-
-	KubernetesConfigFlags = genericclioptions.NewConfigFlags(false)
-	KubernetesConfigFlags.AddFlags(cmd.Flags())
-
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	return cmd
+}
+
+// Complete takes the command arguments and factory and infers any remaining options.
+func Run(f cmdutil.Factory, cmd *cobra.Command, args []string) error {
+	if err := plugin.RunPlugin(f, cmd, args); err != nil {
+		return cmdutil.UsageErrorf(cmd, err.Error())
+	}
+	return nil
 }
 
 func InitAndExecute() {
--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -1,33 +1,174 @@
 package plugin
 
 import (
-	"fmt"
-
-	"github.com/pkg/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/client-go/kubernetes"
+	"encoding/json"
+	"github.com/Masterminds/sprig/v3"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/cli-runtime/pkg/resource"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"os"
+	"syscall"
+	"text/template"
+	"time"
 )
 
-func RunPlugin(configFlags *genericclioptions.ConfigFlags, outputCh chan string) error {
-	config, err := configFlags.ToRESTConfig()
+var funcMap = template.FuncMap{
+	"green":  color.GreenString,
+	"greenBold":  color.New(color.FgGreen, color.Bold).SprintfFunc(),
+	"yellow": color.YellowString,
+	"yellowBold": color.New(color.FgYellow, color.Bold).SprintfFunc(),
+	"red": color.RedString,
+	"redBold": color.New(color.FgRed, color.Bold).SprintfFunc(),
+	"cyan": color.CyanString,
+	"cyanBold": color.HiCyanString,
+	"bold": color.New(color.Bold).SprintfFunc(),
+	"colorAgo": colorAgo,
+	"colorDuration": colorDuration,
+	"dateSub": func (date1, date2 time.Time) time.Duration { return date2.Sub(date1) },
+	"podQosColor": podQosColor,
+	"podPhaseColor": podPhaseColor,
+	"signalName": func (signal int64) string { return unix.SignalName(syscall.Signal(signal)) },
+	"redIf": func (cond bool, str string) string { if cond { return color.RedString(str) }; return str},
+	/*
+	containerReadyColor
+	containerRestartCountColor
+	containerExitCodeColor
+	containerTerminateReasonColor
+	  'red' if reason in ('Error', 'OOMKilled') else 'green'
+	PodReasonColor:
+	  'red' if 'Evicted'
+	podPhaseColor
+	markRed "latest" --> image:tag, paint "latest" in red
+	exitCodeColor
+	toSignalName
+	humanizeBytes
+	*/
+}
+func podQosColor(qos string) string {
+	switch qos {
+	case "BestEffort":
+		return color.RedString(qos)
+	case "Burstable":
+		return color.YellowString(qos)
+	case "Guaranteed":
+		return color.GreenString(qos)
+	default:
+		return color.RedString(qos)
+	}
+}
+
+func podPhaseColor(phase string) string {
+	switch phase {
+	case "Pending":
+		return color.YellowString(phase)
+	case "Running":
+		return color.GreenString(phase)
+	case "Succeeded":
+		return color.GreenString(phase)
+	case "Failed":
+		return color.RedString(phase)
+	case "Unknown":
+		return color.RedString(phase)
+	default:
+		return phase
+	}
+}
+
+func colorAgo(kubeDate string) string {
+	t, _ := time.ParseInLocation("2006-01-02T15:04:05Z", kubeDate, time.Local)
+	duration := time.Since(t).Round(time.Second)
+	return colorDuration(duration)
+}
+
+func colorDuration(duration time.Duration) string {
+	durationRound := (sprig.GenericFuncMap()["durationRound"]).(func(duration interface{}) string)
+	str := durationRound(duration.String())
+	if duration < time.Minute * 5 {
+		return color.RedString(str)
+	}
+	if duration < time.Hour {
+		return color.YellowString(str)
+	}
+	if duration < time.Hour * 24 {
+		return color.MagentaString(str)
+	}
+	return str
+}
+
+func RunPlugin(f cmdutil.Factory, cmd *cobra.Command, args []string) error {
+	//log := logger.NewLogger()
+	//log.Info(strings.Join(args, ","))
+	clientConfig := f.ToRawKubeConfigLoader()
+	namespace, enforceNamespace, err := clientConfig.Namespace()
 	if err != nil {
-		return errors.Wrap(err, "failed to read kubeconfig")
+		return err
 	}
+	filenames := cmdutil.GetFlagStringSlice(cmd, "filename")
 
-	clientset, err := kubernetes.NewForConfig(config)
+	r := f.NewBuilder().
+		Unstructured().
+		NamespaceParam(namespace).DefaultNamespace().AllNamespaces(cmdutil.GetFlagBool(cmd, "all-namespaces")).
+		FilenameParam(enforceNamespace, &resource.FilenameOptions{Filenames: filenames}).
+		LabelSelectorParam(cmdutil.GetFlagString(cmd, "selector")).
+		ResourceTypeOrNameArgs(true, args...).
+		ContinueOnError().
+		Flatten().
+		Do()
+
+	err = r.Err()
 	if err != nil {
-		return errors.Wrap(err, "failed to create clientset")
+		return err
 	}
 
-	namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
-	if err != nil {
-		return errors.Wrap(err, "failed to list namespaces")
+	allErrs := []error{}
+	infos, infosErr := r.Infos()
+	if infosErr != nil {
+		allErrs = append(allErrs, err)
 	}
 
-	for _, namespace := range namespaces.Items {
-		outputCh <- fmt.Sprintf("Namespace %s", namespace.Name)
-	}
+	errs := sets.NewString()
+	for _, info := range infos {
+		var data []byte
+		var err error
+		obj := info.Object
+		data, err = json.Marshal(obj)
+		if err != nil {
+			if errs.Has(err.Error()) {
+				continue
+			}
+			allErrs = append(allErrs, err)
+			errs.Insert(err.Error())
+			continue
+		}
 
-	return nil
+		out := map[string]interface{}{}
+		if err := json.Unmarshal(data, &out); err != nil {
+			if errs.Has(err.Error()) {
+				continue
+			}
+			allErrs = append(allErrs, err)
+			errs.Insert(err.Error())
+			continue
+		}
+		tmpl := template.Must(template.
+			New("pkg/plugin/templates.tmpl").
+			Funcs(sprig.TxtFuncMap()).
+			Funcs(funcMap).
+			ParseFiles("pkg/plugin/templates.tmpl"))
+		kind := info.ResourceMapping().GroupVersionKind.Kind
+		err = tmpl.ExecuteTemplate(os.Stderr, kind, out)
+		if err != nil {
+			if errs.Has(err.Error()) {
+				continue
+			}
+			allErrs = append(allErrs, err)
+			errs.Insert(err.Error())
+			continue
+		}
+	}
+	return utilerrors.NewAggregate(allErrs)
 }
