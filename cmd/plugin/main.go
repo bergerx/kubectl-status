6893f4592a62edcdc28177ab5e565d3eada5a47d
Bekir Dogan <bekirdo@gmail.com>
2021-09-28 15:34:22 +0100

    fix: always use UTC for all internal dates
    fixes #38

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"os"
+
 	"github.com/bergerx/kubectl-status/cmd/plugin/cli"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // required for GKE
 )
 
 func main() {
+	// Kubernetes uses UTC times, status shows times only in ".. ago" format, so
+	// setting the TZ to UTC is safe.
+	os.Setenv("TZ", "UTC")
 	cli.InitAndExecute()
 }
