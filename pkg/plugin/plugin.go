b6a0036dbcf2f6c3e70e00426f437e14c21ae90d
Bekir Dogan <dbekir@vmware.com>
2022-09-25 02:33:24 +0100

    support combined --local and --recursive flags

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/kubectl/pkg/cmd/util"
 	"os"
+	"path/filepath"
 	_ "unsafe" // required for using go:linkname in the file
 
 	"github.com/fatih/color"
@@ -34,8 +35,8 @@ func Run(f util.Factory, args []string) error {
 	}
 	klog.V(5).InfoS("Created engine", "engine", engine)
 	if viper.GetBool("local") {
-		err := runLocal(engine)
-		return err
+		runLocal(engine)
+		return nil
 	}
 	return runRemote(args, engine)
 }
@@ -126,45 +127,75 @@ func processObj(obj runtime.Object, engine *renderEngine) {
 	fmt.Printf("\n")
 }
 
-func runLocal(engine *renderEngine) error {
+func runLocal(engine *renderEngine) {
+	klog.V(5).InfoS("Running locally, will try to avoid doing any requests to the apiserver")
 	for _, filename := range viper.GetStringSlice("filename") {
 		klog.V(5).InfoS("Processing local file", "filename", filename)
-		f, err := os.Open(filename)
-		if err != nil {
-			klog.V(0).ErrorS(err, "Failed to open file", "filename", filename)
-			return err
-		}
-		yr := yaml.NewYAMLReader(bufio.NewReader(f))
-		eof := false
-		for !eof {
-			b, err := yr.Read()
-			if err == io.EOF {
-				klog.V(10).InfoS("Reached end of the file", "filename", filename)
-				eof = true
-			} else if err != nil {
-				klog.V(3).ErrorS(err, "Error reading file", "filename", filename)
-				continue
-			}
-			if len(b) == 0 {
-				continue
-			}
-			klog.V(10).InfoS("Parsing document in the file", "document", b)
-			var out map[string]interface{}
-			err = kyaml.Unmarshal(b, &out)
-			if err != nil {
-				klog.V(3).ErrorS(err, "Error parsing document in the file", "filename", filename)
-				continue
-			}
-			if items, ok := out["items"]; ok {
-				for _, obj := range items.([]interface{}) {
-					renderObj(engine, obj.(map[string]interface{}))
+		if viper.GetBool("recursive") {
+			klog.V(5).InfoS("Processing recursive", "filename", filename)
+			filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+				klog.V(5).InfoS("Processing local file", "path", path)
+				if err != nil {
+					klog.V(10).ErrorS(err, "filepath walk error", "path", path, "info", info)
+					return nil
 				}
-			} else {
-				renderObj(engine, out)
-			}
+				if info.IsDir() {
+					return nil
+				}
+				runLocalForFile(engine, path)
+				return nil
+			})
+		} else {
+			runLocalForFile(engine, filename)
 		}
 	}
-	return nil
+}
+
+func runLocalForFile(engine *renderEngine, filename string) {
+	klog.V(5).InfoS("Processing local file", "filename", filename)
+	fi, err := os.Stat(filename)
+	if err != nil {
+		klog.V(0).ErrorS(err, "Failed to stat file", "filename", filename)
+		return
+	}
+	if fi.IsDir() {
+		klog.V(0).InfoS("A folder provided without --recursive flag", "filename", filename)
+		return
+	}
+	f, err := os.Open(filename)
+	if err != nil {
+		klog.V(0).ErrorS(err, "Failed to open file", "filename", filename)
+		return
+	}
+	yr := yaml.NewYAMLReader(bufio.NewReader(f))
+	eof := false
+	for !eof {
+		b, err := yr.Read()
+		if err == io.EOF {
+			klog.V(10).InfoS("Reached end of the file", "filename", filename)
+			eof = true
+		} else if err != nil {
+			klog.V(3).ErrorS(err, "Error reading file", "filename", filename)
+			continue
+		}
+		if len(b) == 0 {
+			continue
+		}
+		klog.V(10).InfoS("Parsing document in the file", "document", b)
+		var out map[string]interface{}
+		err = kyaml.Unmarshal(b, &out)
+		if err != nil {
+			klog.V(3).ErrorS(err, "Error parsing document in the file", "filename", filename)
+			continue
+		}
+		if items, ok := out["items"]; ok {
+			for _, obj := range items.([]interface{}) {
+				renderObj(engine, obj.(map[string]interface{}))
+			}
+		} else {
+			renderObj(engine, out)
+		}
+	}
 }
 
 func renderObj(engine *renderEngine, out map[string]interface{}) {
