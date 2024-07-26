e97b608d9c7ee0eb65c8aac9203305d3af4da3e0
Bekir Dogan <dbekir@vmware.com>
2024-07-26 02:47:25 +0100

    improve cmd tests

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -13,44 +13,59 @@ import (
 	"github.com/bergerx/kubectl-status/pkg/plugin"
 )
 
+type cmdTest struct {
+	name    string
+	args    []string
+	stdout  string // Regex
+	stderr  string // Regex
+	wantErr string // Contains
+}
+
+func (c cmdTest) assert(t *testing.T) {
+	t.Helper()
+	stdout, stderr, err := executeCMD(c.args)
+	if c.stdout == "" {
+		assert.Empty(t, stdout)
+	} else {
+		assert.Regexp(t, c.stdout, stdout)
+	}
+	if c.stderr == "" {
+		assert.Empty(t, stderr)
+	} else {
+		assert.Regexp(t, c.stderr, stderr)
+	}
+	if c.wantErr != "" {
+		assert.ErrorContains(t, err, c.wantErr)
+	}
+}
+
 func TestRootCmdWithoutACluster(t *testing.T) {
 	_ = os.Setenv("KUBECONFIG", "/dev/null")
 	defer plugin.SetDurationRound(func(_ interface{}) string { return "1m" })()
-	tests := []struct {
-		name    string
-		args    []string
-		stdout  string
-		stderr  string
-		wantErr string
-	}{
+	tests := []cmdTest{
 		{
 			name:   "empty call should print an error and simple usage",
-			stdout: `^$`,
 			stderr: `You must provide one or more resources by argument or filename.\nExample resource specifications`,
 		},
 		{
 			name:   "pods against a non-configured client should print an error",
 			args:   []string{"pods"},
-			stdout: `^$`,
 			stderr: `The connection to the server localhost:8080 was refused`,
 		},
 		{
 			name:   "missing file should fail",
 			args:   []string{"-f", "non-existing.yaml"},
-			stdout: `^$`,
 			stderr: `error: the path \"non-existing.yaml\" does not exist\n$`,
 		},
 		{
 			name:   "file without local should fail",
 			args:   []string{"-f", "../tests/artifacts/deployment-healthy.yaml"},
-			stdout: `^$`,
 			stderr: `dial tcp \[::1\]:8080: connect: connection refused\n$`,
 		},
 		{
 			name:   "file with local should succeed",
 			args:   []string{"-f", "../tests/artifacts/deployment-healthy.yaml", "--local"},
 			stdout: `^\nDeployment/httpbin-deployment`,
-			stderr: `^$`,
 		},
 		{
 			name: "cr file with local with status should render ready conditions",
@@ -60,36 +75,23 @@ DatabaseConnection/mymysql -n default, created 1m ago
   Current: Resource is Ready
   Ready ConnectionEstablished, Database connection successfully established. for 1m
 $`,
-			stderr: `^$`,
 		},
 		{
 			name:   "file with kind: List should list both resources",
 			args:   []string{"-f", "../tests/artifacts/multiple-2-pods-list.yaml", "--local"},
 			stdout: `(?ms)Pod/etcd-minikube.*Pod/storage-provisioner`,
-			stderr: `^$`, // TODO: prints `couldn't get current server API group list: Get "http://localhost:8080/api?timeout=32s": dial tcp [::1]:8080: connect: connection refused`
+			// TODO: logs `couldn't get current server API group list: Get "http://localhost:8080/api?timeout=32s": dial tcp [::1]:8080: connect: connection refused`
 		},
 		{
 			name:   "file with multiple yaml documents should list all resources",
 			args:   []string{"-f", "../tests/artifacts/multiple-2-pods-docs.yaml", "--local"},
 			stdout: `(?ms)Pod/etcd-minikube.*Pod/storage-provisioner`,
-			stderr: `^$`, // TODO: prints `couldn't get current server API group list: Get "http://localhost:8080/api?timeout=32s": dial tcp [::1]:8080: connect: connection refused`
+			// TODO: logs `couldn't get current server API group list: Get "http://localhost:8080/api?timeout=32s": dial tcp [::1]:8080: connect: connection refused`
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.args == nil {
-				tt.args = []string{}
-			}
-			stdout, stderr, err := execute(tt.args)
-			if tt.stdout != "" {
-				assert.Regexp(t, tt.stdout, stdout)
-			}
-			if tt.stderr != "" {
-				assert.Regexp(t, tt.stderr, stderr)
-			}
-			if tt.wantErr != "" {
-				assert.ErrorContains(t, err, tt.wantErr)
-			}
+			tt.assert(t)
 		})
 	}
 }
@@ -102,63 +104,40 @@ func TestE2EWithVanillaMinikube(t *testing.T) {
 	defer plugin.SetDurationRound(func(_ interface{}) string { return "1m" })()
 	klog.InitFlags(nil)
 	t.Log("starting tests...")
-	tests := []struct {
-		name    string
-		args    []string
-		stdout  string
-		stderr  string
-		wantErr string
-	}{
+	tests := []cmdTest{
 		{
 			name:   "empty call should print an error and usage",
-			stdout: `^$`,
 			stderr: `You must provide one or more resources by argument or filename.\nExample resource specifications`,
 		},
 		{
 			name:   "pods on non-existent ns should fail",
 			args:   []string{"pods", "-n", "non-existent"},
-			stdout: `^$`,
 			stderr: `error: no resources found\n$`,
 		},
 		{
 			name:   "pods on kube-system ns should return storage-provisioner",
 			args:   []string{"pods", "-n", "kube-system"},
 			stdout: `^\nPod/[a-z0-9-]+ -n kube-system`,
-			stderr: `^$`,
 		},
 		{
 			name:   "node query should return return at least a node",
 			args:   []string{"node"},
 			stdout: `^\nNode/`,
-			stderr: `^$`,
 		},
 		{
 			name:   "cr file without a crd should fail",
 			args:   []string{"-f", "../tests/artifacts/cr-dbconn-mymysql.yaml"},
-			stdout: `^$`,
 			stderr: `no matches for kind "DatabaseConnection" in version "example.com/v1alpha1"`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.args == nil {
-				tt.args = []string{}
-			}
-			stdout, stderr, err := execute(tt.args)
-			if tt.stdout != "" {
-				assert.Regexp(t, tt.stdout, stdout)
-			}
-			if tt.stderr != "" {
-				assert.Regexp(t, tt.stderr, stderr)
-			}
-			if tt.wantErr != "" {
-				assert.ErrorContains(t, err, tt.wantErr)
-			}
+			tt.assert(t)
 		})
 	}
 }
 
-func execute(args []string) (string, string, error) {
+func executeCMD(args []string) (string, string, error) {
 	cmd := RootCmd()
 	stdout := &bytes.Buffer{}
 	cmd.SetOut(stdout)
